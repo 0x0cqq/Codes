b14c1244ac78494b2524c4d81f4354ddc486ecbe
Chen Qiqian <qiqianchen@gmail.com>
2018-02-08 20:21:14 +0800

    edit p3376

--- luogu/p3376_2.cpp
+++ luogu/p3376_2.cpp
@@ -1,25 +1,16 @@
 #include <cstdio>
-#include <unistd.h>
 #include <queue>
 #include <cstring>
 #include <algorithm>
 using namespace std;
 
-void qr(int &x){
-    x = 0;char ch = getchar();
-    while(ch<'0'||ch>'9')
-        ch = getchar();
-    while(ch>='0'&&ch<='9')
-        x = (x<<3)+(x<<1)+ch-48,ch = getchar();
-}
-
 struct Edge{
     int from,to,flow,cap;
     int next;
 }edge[201000];
-
 int fir[10100],dis[10100],cur[10100];
-int n,m,T,home[10100],local[10100],s,t,tot = 2;
+
+int n,m,s,t,tot = 2;
 
 bool bfs(){
     queue<int> q;
@@ -28,62 +19,57 @@ bool bfs(){
     q.push(s);dis[s] = 1;
     while(!q.empty()){
         int nown = q.front();q.pop();
-        for(int e = fir[nown];e!=0;e = edge[e].next){
-            int v = edge[e].to;
-            if(dis[v] == 0 && edge[e].cap > edge[e].flow){
+        for(int nowe = fir[nown];nowe!=0;nowe = edge[nowe].next){
+            int v = edge[nowe].to;
+            if(dis[v] == 0 && edge[nowe].cap > edge[nowe].flow){
                 dis[v] = dis[nown]+1;
                 q.push(v);
+                if(v == t)
+                    return dis[t];
             }
         }
     }
     return dis[t];
 }
 
-int dfs(int nown,int limit = 2147483647){
-    if(nown == t) return limit;
-    if(limit == 0) return 0;
-    int fff = 0;
-    for(int &e = cur[nown];e!=0;e = edge[e].next){
-        int f,v = edge[e].to;
-        if(dis[v] == dis[nown]+1 && (f = dfs(v,min(limit,edge[e].cap-edge[e].flow)))>0){
-            edge[e].flow+=f;
-            edge[e^1].flow-=f;
-            limit-=f,fff+=f;
-            if(limit == 0) break;
+int dfs(int nown,int limit = 0x3f3f3f3f){
+    if(nown == t || limit == 0)
+        return limit;
+    for(int &nowe = cur[nown];nowe!=0;nowe = edge[nowe].next){
+        int v = edge[nowe].to;
+        if(dis[v] == dis[nown]+1 && edge[nowe].flow < edge[nowe].cap){
+            int f = dfs(v,min(edge[nowe].cap-edge[nowe].flow,limit));
+            if(f>0){
+                edge[nowe].flow+=f;
+                edge[nowe^1].flow-=f;
+                return f;
+            }
         }
     }
-    return fff;
-}
-
-void print(){
-    for(int i = 1;i <= t;i++)
-        printf("i:%d dis:%d,cur:%d\n",i,dis[i],cur[i]);
-    for(int i = 0;i<tot;i++)
-        printf("u:%d v:%d f:%d c:%d\n",edge[i].from,edge[i].to,edge[i].flow,edge[i].cap);
+    return 0;
 }
 
 int dinic(){
-    int ans = 0;
+    int ans = 0,f;
     while(bfs()){
-        ans += dfs(s);
-        //printf("ANS:%d\n",ans);print();sleep(2);
+        while((f = dfs(s))>0)
+            ans+=f;
     }
     return ans;
 }
 
-void addedge(int from,int to,int c){
-    edge[tot].from = from;edge[tot].to = to;
+void addedge(int a,int b,int c){
+    edge[tot].from = a;edge[tot].to = b;
     edge[tot].cap = c;edge[tot].flow = 0;
-    edge[tot].next = fir[from];fir[from] = tot;
+    edge[tot].next = fir[a];fir[a] = tot;
     tot++;
 }
 
 int main(){
-    //freopen("1.in","r",stdin);
-    qr(n),qr(m),qr(s),qr(t);
+    scanf("%d %d %d %d",&n,&m,&s,&t);
     for(int i = 0;i<m;i++){
         int a,b,c;
-        qr(a),qr(b),qr(c);
+        scanf("%d %d %d",&a,&b,&c);
         addedge(a,b,c);
         addedge(b,a,0);
     }
