30ff95215a78a5a56915674ec46219d8b1ad7a39
Chen Qiqian <qiqianchen@gmail.com>
2018-03-13 20:00:23 +0800

    upd

--- luogu/p2023.cpp
+++ luogu/p2023.cpp
@@ -1,27 +1,79 @@
 #include <cstdio>
+#include <cctype>
+#define int long long
 #define ll long long
 #define lson (nown<<1)
 #define rson (nown<<1|1)
 #define mid ((l+r)>>1)
-#pragma GCC optimize(2)
+#pragma GCC optimize(3)
 using namespace std;
+
+namespace fast_io {
+    inline char read();
+    inline void read(int&);
+//  inline void read(char*);
+    inline void print(char);
+    inline void print(int);
+//  inline void print(char*);
+    inline void flush();
+    inline char read() {
+//		return getchar();
+        static const int IN_LEN = 1000000;
+        static char buf[IN_LEN], *s, *t;
+        return s==t?(((t=(s=buf)+fread(buf,1,IN_LEN,stdin))== s)?-1:*s++) : *s++;
+    }
+    inline void read(int &x) {
+        static bool iosig;
+        static char c;
+        for (iosig = false, c = read(); !isdigit(c); c = read()) {
+            if (c == '-') iosig = true;
+            if (c == -1) return;
+        }
+        for (x = 0; isdigit(c); c = read())
+            	x = ((x+(x<<2))<<1) + (c ^ '0');
+        if (iosig) x = -x;
+    }
+    /*
+    inline void read(char *a){
+        static char c = read();
+        while(c!= -1&&(c == ' '||c =='\n')){
+            c = read();
+        }
+        while(c!= -1&&c!=' '&&c!='\n'){
+            *a++ = c;
+            c = read();
+        }
+        *a = 0;
+    }*/
+    const int OUT_LEN = 1000000;
+    char obuf[OUT_LEN], *ooh = obuf;
+    inline void print(char c) {
+        if (ooh == obuf + OUT_LEN) fwrite(obuf, 1, OUT_LEN, stdout), ooh = obuf;
+        *ooh++ = c;
+    }
+    inline void print(int x) {
+        static int buf[30], cnt;
+        if (x == 0) {
+            print('0');
+        }
+        else {
+            if (x < 0) print('-'), x = -x;
+            for (cnt = 0; x; x /= 10) buf[++cnt] = x % 10 + 48;
+            while (cnt) print((char)buf[cnt--]);
+        }
+    }
+    /*
+    inline void print(char *a){
+        while(*a) print(*a++);
+    }
+    */
+    inline void flush() {
+        fwrite(obuf, 1, ooh - obuf, stdout);
+    }
+}using namespace fast_io;
 
 ll addn[500000],chengn[500000],sumn[500000];
 ll n,m,p;
-
-inline ll qr(){
-    ll f = 1,s = 0;char ch = getchar();
-    while(ch<'0'||ch>'9'){
-        if(ch == '-')
-            f = -1;
-        ch = getchar();
-    }
-    while(ch>='0'&&ch<='9'){
-        s = (s<<3)+(s<<1)+ch-48;
-        ch = getchar();
-    }
-    return f*s;
-}
 
 void pushup(int nown){
     sumn[nown] = (sumn[lson]+sumn[rson])%p;
@@ -48,7 +100,8 @@ void pushdown(int nown,int l,int r){
 void build(int nown,int l,int r){
     addn[nown] = 0;chengn[nown] = 1;
     if(l == r){
-        sumn[nown] = qr()%p;
+        read(sumn[nown]);
+        sumn[nown] %= p;
         return;
     }
     build(lson,l,mid);
@@ -83,7 +136,7 @@ ll query(int nown,int l,int r,int ql,int qr){
     if(l>qr||r<ql)
         return 0;
     if(ql<=l&&r<=qr){
-        return sumn[nown]%p;
+        return sumn[nown] % p;
     }
     pushdown(nown,l,r);
     ll ans = 0;
@@ -95,25 +148,25 @@ ll query(int nown,int l,int r,int ql,int qr){
 }
 
 
-int main(){
-    scanf("%lld %lld",&n,&p);
+main(){
+    read(n),read(p);
     build(1,1,n);
-    scanf("%lld",&m);
+    read(m);
     for(int i = 0;i<m;i++){
         ll a,b,c,d;
-        a = qr();
+        read(a);read(b),read(c);
         if(a == 1){
-            b =qr();c = qr();d = qr();
+            read(d);
             update(d,1,1,n,b,c,0);
         }
         else if(a == 2){
-            b =qr();c = qr();d = qr();
+            read(d);
             update(d,1,1,n,b,c,1);
         }
         else if(a == 3){
-            b = qr();c = qr();
-            printf("%lld\n",query(1,1,n,b,c));
+            print(query(1,1,n,b,c)),print('\n');
         }
     }
+    flush();
     return 0;
 }
