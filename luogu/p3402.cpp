615068091e2224339e5db5b718f449b092a19d96
Chen Qiqian <qiqianchen@gmail.com>
2018-05-23 21:13:44 +0800

    upd

--- luogu/p3402.cpp
+++ luogu/p3402.cpp
@@ -1,45 +1,17 @@
 #include <cstdio>
-//#include <algorithm>
-//#include <bitset>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-namespace normal_io{
-    inline char read(){return getchar();}
-    inline void read(int &x){scanf("%d",&x);}
-    inline void read(char *x){scanf("%s",x);}
-    inline void print(int x){printf("%d",x);}
-    inline void print(char x){putchar(x);}
-    inline void flush(){return;}
-}using namespace normal_io;
-
-const int MAXN = 210000;
+const int MAXN = 110000;
 
 namespace prSegTree{
-    int val[MAXN],ls[MAXN],rs[MAXN],cnt = 0;
-    void insert(int &nown,int pre,int l,int r,int pos,int d){
-        nown = ++cnt;ls[nown] = ls[pre],rs[nown] = rs[pre];
-        if(l == r){
-            val[nown] = d
-        }
-        else{
-            if(pos <= mid)
-        }
-    }
-};
+    int val[MAXN],ls[MAXN],rs[MAXN],cnt = 1;
 
-
-struct Edge{
-    int a,b,c;
 }
 
-
-Edge wwww;
-
-
-
 int main(){
-    scanf("%d",x);
-    printf("%d"123123123);
-    wwww.
+    
     return 0;
 }
