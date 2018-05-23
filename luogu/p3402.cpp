fca8a04d015f136517e1fcd0b040978ce1c6d96b
Chen Qiqian <qiqianchen@gmail.com>
2018-05-23 21:32:12 +0800

    upd

--- luogu/p3402.cpp
+++ luogu/p3402.cpp
@@ -1,14 +1,14 @@
 #include <cstdio>
-#include <algorithm>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
 const int MAXN = 110000;
 
 namespace prSegTree{
     int val[MAXN],ls[MAXN],rs[MAXN],cnt = 1;
-
+    int insert(int &nown,int pre,int l,int r,int pos,int d){
+        
+        return 0;
+    }
 }
 
 int main(){
--- luogu/utest.cpp
+++ luogu/utest.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 
 int main(){
-    int n = 10000;
+    int n = 100000;
     int ans = 0;
     for(int i = 1;i<=n;i++){
         for(int j = 1;j<=n;j++){
-            //ans++;
+            ans++;
         }
     }
     return 0;
