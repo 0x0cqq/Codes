ed18f07eb020f0cc0b2beed81cf7d4e019fd9319
ChenQiqian <qiqianchen@gmail.com>
2018-09-03 17:58:21 +0800

    upd

--- loj/loj6278.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-#include <cmath>
-using namespace std;
-
-const int MAXN = 50000;
-
-int v1[MAXN],tag[MAXN];
--- luogu/p2801.cpp
+++ luogu/p2801.cpp
@@ -1,37 +1,29 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
 using namespace std;
 
 const int MAXN = 1100000,MAXQ = 50000;
 
-namespace fast_io{
-    static const int SIZE=1024*1024;char ibuf[SIZE],*s,*t,obuf[SIZE],*oh=obuf;inline void flush(){fwrite(obuf,1,oh-obuf,stdout);}
-    inline char read(){if(s==t)t=(s=ibuf)+fread(ibuf,1,SIZE,stdin);return s==t?-1:*s++;}
-    template<typename T>inline void read(T&x){static char c;static bool iosig;for(iosig=0,c=read();!isdigit(c);c=read()){if(c==-1)return;iosig|=(c=='-');}for(x=0;isdigit(c);c=read())x=(((x<<2)+x)<<1)+(c^48);if(iosig)x=-x;return;}
-    inline void read(char*a){static char c=read();while(c!=-1&&(c==' '||c!='\r'||c=='\n'))c=read();while(c!=-1&&c!=' '&&c!='\r'&&c!='\n')*a++=c,c=read();*a=0;}
-    inline void print(char c){if(oh==obuf+SIZE)fwrite(obuf,1,SIZE,stdout),oh=obuf;*oh++=c;}
-    template<typename T>inline void print(T x){static int buf[21],cnt;if(!x)print('0');else{if(x<0)print('-'),x=-x;for(cnt=0;x;x/=10)buf[++cnt]=x%10+48;while(cnt)print((char)buf[cnt--]);}}
-    inline void print(char*a){while(*a)print(*a++);}
-}using namespace fast_io;
+void read(int &x){
+    scanf("%d",&x);
+}
+
+void read(char *x){
+    scanf("%s",x);
+}
 
 int n,m,num[MAXN];
 
-namespace fenkuai{
-    int q,lb[MAXQ],rb[MAXQ],b[MAXN];//[lb,rb]
-    int num[MAXN];
-    void init(){
-        q = sqrt(n);
-        for(int i = 1,j=1;i+q-1<=n;i+=q,j++){
-            lb[j] = i,rb[j] = i+q-1;
-            for(int w = lb[j];w<=rb[j];w++) b[w] = j;
-        }
-    }
-}
-
 void init(){
     read(n),read(m);
-    for(int i = 1;i<=n;i++){
+    for(int i = 0;i<n;i++)
         read(num[i]);
-    }
+}
+
+void build(){
+
 }
 
 void solve(){
