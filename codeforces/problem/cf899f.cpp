d17c12a85737c97b4e259a1c963601a2eb159490
ChenQiqian <qiqianchen@gmail.com>
2018-12-08 16:24:37 +0800

    add

---
+++ codeforces/problem/cf899f.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 210000;
+
+int march(char x){
+  if(isdigit()){
+    asd
+  }
+}
+
+int n,m;
+char _s[MAXN];
+int s[MAXN];
+
+void init(){
+  scanf("%d %d",&n,&m);
+  scanf("%s",s+1);
+  for(int i = 1;i<=n;i++){
+
+  }
+}
+
+int main(){
+  return 0;
+}
